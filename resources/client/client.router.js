d9b4195b743e9079def9b3f165fdd1aff84025ca
Petro Krevenets <peter.k.nets@gmail.com>
2024-03-11 20:08:36 +0200

    Clean up Client routes (#23)

--- resources/client/client.router.js
+++ resources/client/client.router.js
@@ -10,24 +10,33 @@ router.use(protect);
 
 router
   .route("/")
-  // @route     GET clients/
-  // @desc      Get all clients
-  // @access    Private
+  /**
+   * @route     GET clients/
+   * @desc      Get all clients
+   * @access    Private
+   */
   .get(clientControllers.getAll)
-  // @route     POST clients/
-  // @desc      Create client
-  // @access    Private
+  /**
+   * @route     POST clients/
+   * @desc      Create client
+   * @access    Private
+   */
   .post(clientControllers.createOne);
 
 router
-  .route("/withprojectdata")
+  .route("/withProjectData")
   /**
-   * @route     GET projects/withprojectdata
+   * @route     GET projects/withProjectData
    * @desc      Get projects with project data
    * @access    Private
    */
   .get(
     catchAsync(async (req, res) => {
+      const currentDate = new Date();
+      const last30Days = new Date(currentDate - 30 * 24 * 60 * 60 * 1000);
+      const last90Days = new Date(currentDate - 90 * 24 * 60 * 60 * 1000);
+      const last365Days = new Date(currentDate - 365 * 24 * 60 * 60 * 1000);
+
       const pipeline = [
         //* Match projects that are not deleted
         {
@@ -48,7 +57,21 @@ router
         {
           $unwind: "$clientDetails",
         },
-        //* Group projects by client and calculate aggregated data
+        //* Add fields to calculate number of projects for different time periods
+        {
+          $addFields: {
+            projectsLast30Days: {
+              $cond: { if: { $gte: ["$date", last30Days] }, then: 1, else: 0 },
+            },
+            projectsLast90Days: {
+              $cond: { if: { $gte: ["$date", last90Days] }, then: 1, else: 0 },
+            },
+            projectsLast365Days: {
+              $cond: { if: { $gte: ["$date", last365Days] }, then: 1, else: 0 },
+            },
+          },
+        },
+        //* Group projects by client and calculate sums
         {
           $group: {
             _id: "$client",
@@ -57,56 +80,12 @@ router
             firstProjectDate: { $min: "$date" },
             lastProjectDate: { $max: "$date" },
             totalEarnings: { $sum: "$payment" },
-            projectsLast30Days: {
-              $sum: {
-                $cond: {
-                  if: {
-                    $gte: [
-                      "$date",
-                      {
-                        $subtract: [new Date(), 30 * 24 * 60 * 60 * 1000],
-                      },
-                    ],
-                  },
-                  then: 1,
-                  else: 0,
-                },
-              },
-            },
-            projectsLast90Days: {
-              $sum: {
-                $cond: {
-                  if: {
-                    $gte: [
-                      "$date",
-                      {
-                        $subtract: [new Date(), 90 * 24 * 60 * 60 * 1000],
-                      },
-                    ],
-                  },
-                  then: 1,
-                  else: 0,
-                },
-              },
-            },
-            projectsLast365Days: {
-              $sum: {
-                $cond: {
-                  if: {
-                    $gte: [
-                      "$date",
-                      {
-                        $subtract: [new Date(), 365 * 24 * 60 * 60 * 1000],
-                      },
-                    ],
-                  },
-                  then: 1,
-                  else: 0,
-                },
-              },
-            },
+            projectsLast30Days: { $sum: "$projectsLast30Days" },
+            projectsLast90Days: { $sum: "$projectsLast90Days" },
+            projectsLast365Days: { $sum: "$projectsLast365Days" },
           },
         },
+        //* Choose what data to return
         {
           $project: {
             _id: 1,
@@ -122,7 +101,7 @@ router
               $trunc: {
                 $divide: [
                   {
-                    $subtract: [new Date(), "$lastProjectDate"],
+                    $subtract: [currentDate, "$lastProjectDate"],
                   },
                   24 * 60 * 60 * 1000, // Convert milliseconds to days
                 ],
@@ -139,22 +118,27 @@ router
         data: result,
       });
     }),
-  )
-  .get();
+  );
 
 router
   .route("/:id")
-  // @route     GET clients/:id
-  // @desc      Get client
-  // @access    Private
+  /**
+   * @route     GET clients/:id
+   * @desc      Get client
+   * @access    Private
+   */
   .get(clientControllers.getOne)
-  // @route     PATCH clients/:id
-  // @desc      Update client
-  // @access    Private
+  /**
+   * @route     PATCH clients/:id
+   * @desc      Update client
+   * @access    Private
+   */
   .patch(clientControllers.updateOne)
-  // @route     DELETE clients/:id
-  // @desc      Delete client
-  // @access    Private
+  /**
+   * @route     DELETE clients/:id
+   * @desc      Delete client
+   * @access    Private
+   */
   .delete(clientControllers.deleteOne);
 
 module.exports = router;
