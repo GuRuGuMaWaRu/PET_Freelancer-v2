6645e53910f046172461af06a14c6dc4e34a0474
Petro Krevenets <peter.k.nets@gmail.com>
2024-03-14 20:35:10 +0200

    24-refactor/improve user routes (#25)

--- resources/user/user.router.js
+++ resources/user/user.router.js
@@ -8,8 +8,19 @@ const userControllers = require("./user.controllers");
 
 const router = express.Router();
 
-// Authorization routes
+//* Authorization routes
+/**
+ * @route     GET getUser/
+ * @desc      Get my user details
+ * @access    Private
+ */
 router.get("/getUser", protect, authControllers.getUser);
+
+/**
+ * @route     POST login/
+ * @desc      Log in with username and password
+ * @access    Public
+ */
 router.post(
   "/login",
   loginLimiter,
@@ -20,13 +31,19 @@ router.post(
   validateForm,
   authControllers.login,
 );
+
+/**
+ * @route     POST signup/
+ * @desc      Register a new user
+ * @access    Public
+ */
 router.post(
   "/signup",
   [
     check("name", "Please add name")
       .not()
       .isEmpty(),
-    check("email", "Please add email").isEmail(),
+    check("email", "Please provide valid email").isEmail(),
     check(
       "password",
       "Please enter a password with 6 or more characters",
@@ -36,25 +53,31 @@ router.post(
   authControllers.signup,
 );
 
-//--> Admin routes
+//* Admin routes
 router.use(protect);
 
 router
   .route("/")
-  // @route     GET users/
-  // @desc      Get all users
-  // @access    Private
+  /**
+   * @route     GET users/
+   * @desc      Get all users
+   * @access    Private
+   */
   .get(userControllers.getAll);
 
 router
   .route("/:id")
-  // @route     GET users/:id
-  // @desc      Get user
-  // @access    Private
+  /**
+   * @route     GET users/:id
+   * @desc      Get user
+   * @access    Private
+   */
   .get(userControllers.getOne)
-  // @route     DELETE users/:id
-  // @desc      Delete user
-  // @access    Private
+  /**
+   * @route     DELETE users/:id
+   * @desc      Delete user
+   * @access    Private
+   */
   .delete(userControllers.deleteOne);
 
 module.exports = router;
