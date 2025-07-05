0973584b2962dbfc7a8b05854d7bdb9637bbc46e
Petro Krevenets <petro.krevenets@solcellespesialisten.no>
2025-07-05 13:50:50 +0300

    Pass ref to inputs

--- client/src/shared/ui/FormComponents/ui/Input/Input.tsx
+++ client/src/shared/ui/FormComponents/ui/Input/Input.tsx
@@ -1,17 +1,25 @@
+import React from "react";
 import styles from "./Input.module.css";
 
-const SInput = (props: React.InputHTMLAttributes<HTMLInputElement>) => {
-  return <input className={styles.inputBase} {...props} />;
-};
+const SInput = React.forwardRef<
+  HTMLInputElement,
+  React.InputHTMLAttributes<HTMLInputElement>
+>((props, ref) => {
+  return <input className={styles.inputBase} ref={ref} {...props} />;
+});
 
-const SSelect = (props: React.SelectHTMLAttributes<HTMLSelectElement>) => {
-  return <select className={styles.inputBase} {...props} />;
-};
+const SSelect = React.forwardRef<
+  HTMLSelectElement,
+  React.SelectHTMLAttributes<HTMLSelectElement>
+>((props, ref) => {
+  return <select className={styles.inputBase} ref={ref} {...props} />;
+});
 
-const STextarea = (
-  props: React.TextareaHTMLAttributes<HTMLTextAreaElement>
-) => {
-  return <textarea className={styles.inputBase} {...props} />;
-};
+const STextarea = React.forwardRef<
+  HTMLTextAreaElement,
+  React.TextareaHTMLAttributes<HTMLTextAreaElement>
+>((props, ref) => {
+  return <textarea className={styles.inputBase} ref={ref} {...props} />;
+});
 
 export { SInput, SSelect, STextarea };
