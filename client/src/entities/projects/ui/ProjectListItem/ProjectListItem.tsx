bf2f225b420ae0099f677653564dcb2c55eff6ca
Petro Krevenets <petro.krevenets@solcellespesialisten.no>
2025-07-21 12:25:47 +0300

    Migrate ProjectListItem to css-modules

--- client/src/entities/projects/ui/ProjectListItem/ProjectListItem.tsx
+++ client/src/entities/projects/ui/ProjectListItem/ProjectListItem.tsx
@@ -1,30 +1,49 @@
 import React from "react";
+import clsx from "clsx";
 
 import type { IProject } from "shared/types";
-import { SDataCell } from "./ProjectListItem.styles";
+
+import styles from "./ProjectListItem.module.css";
 
 interface IProps {
   project: IProject;
   children: React.ReactNode;
 }
 
+//** TODO: it may be a good idea to transfer this to html table, makes more sense to me now */
+
 const ProjectListItem: React.FC<IProps> = ({ project, children }) => {
   return (
     <>
-      <SDataCell name="client">{project.client.name}</SDataCell>
-      <SDataCell name="date">
+      <div className={styles.dataCell} data-name="client">
+        {project.client.name}
+      </div>
+      <div className={clsx(styles.dataCell, styles.date)} data-name="date">
         <time>{new Date(project.date).toLocaleDateString("default")}</time>
-      </SDataCell>
-      <SDataCell name="project nr">{project.projectNr}</SDataCell>
-      <SDataCell name="payment" payment={project.payment}>
+      </div>
+      <div className={styles.dataCell} data-name="project nr">
+        {project.projectNr}
+      </div>
+      <div
+        className={clsx(
+          styles.dataCell,
+          project.payment === 0 && styles.paymentZero
+        )}
+        data-name="payment"
+      >
         {project.payment}
-      </SDataCell>
-      <SDataCell name="comments">
+      </div>
+      <div
+        className={clsx(styles.dataCell, styles.comments)}
+        data-name="comments"
+      >
         {project.comments && project.comments?.length > 30
           ? project.comments.slice(0, 30) + "..."
           : project.comments}
-      </SDataCell>
-      <SDataCell name="actions">{children}</SDataCell>
+      </div>
+      <div className={styles.dataCell} data-name="actions">
+        {children}
+      </div>
     </>
   );
 };
