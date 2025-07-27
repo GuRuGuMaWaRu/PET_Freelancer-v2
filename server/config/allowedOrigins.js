1943cdaf86d316009defe8b4307ecb157765a6a2
Petro Krevenets <petro.krevenets@solcellespesialisten.no>
2025-07-27 10:51:57 +0300

    fix: change allowed origins

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,9 +1,9 @@
 const allowedOrigins = [
-  "http://localhost:3000/",
-  "http://localhost:5000/",
-  "http://localhost:5005/",
-  "https://me-freelancer-v1-duml.onrender.com/",
-  "https://tranquil-woodland-50991.herokuapp.com/",
+  "http://localhost:3000",
+  "http://localhost:5000",
+  "http://localhost:5005",
+  "https://me-freelancer-v1-duml.onrender.com",
+  "https://tranquil-woodland-50991.herokuapp.com",
 ];
 
 module.exports = allowedOrigins;
