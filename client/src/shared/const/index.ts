8e8afff7fd8d45ee57fd6a28d4186a48b4c42691
Petro Krevenets <petro.krevenets@solcellespesialisten.no>
2025-07-27 11:58:08 +0300

    refactor: remove unused mq constants

--- client/src/shared/const/index.ts
+++ client/src/shared/const/index.ts
@@ -1,3 +1,2 @@
 export * as colors from "./colors";
 export * as config from "./config";
-export * as mq from "./media-queries";
--- client/src/shared/const/media-queries.ts
+++
@@ -1,3 +0,0 @@
-export const large = "@media (min-width: 1300px)";
-export const medium = "@media (max-width: 1299px)";
-export const small = "@media (max-width: 691px)";
